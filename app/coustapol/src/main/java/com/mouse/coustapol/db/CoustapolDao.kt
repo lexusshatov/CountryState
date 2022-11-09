c1a5cff8633894d43490e8b0a6546b86d3324799
Oleh Shatov <lexusshatov2@gmail.com>
2022-11-09 13:32:34 +0200

    Search by AlternateNames

--- app/coustapol/src/main/java/com/mouse/coustapol/db/CoustapolDao.kt
+++ app/coustapol/src/main/java/com/mouse/coustapol/db/CoustapolDao.kt
@@ -15,8 +15,8 @@ interface CoustapolDao {
     fun findCountry(query: String): Country?
 
     @Query("SELECT * FROM State " +
-            "WHERE UPPER(State.Country)=(SELECT Abbreviate FROM Country WHERE UPPER(Name)=UPPER(:country) OR UPPER(Abbreviate)=UPPER(:country)) " +
-            "AND (UPPER(Name)=UPPER(:query) OR UPPER(Abbreviate)=UPPER(:query))")
+            "WHERE UPPER(State.Country)=UPPER((SELECT Abbreviate FROM Country WHERE UPPER(Name)=UPPER(:country) OR UPPER(Abbreviate)=UPPER(:country))) " +
+            "AND (UPPER(Name)=UPPER(:query) OR UPPER(Abbreviate)=UPPER(:query) OR INSTR(UPPER(AlternateNames), UPPER(:query)) > 0)")
     fun findState(country: String, query: String): State?
 
     @Query("SELECT * FROM State WHERE UPPER(State.Country)=UPPER(:country)")
