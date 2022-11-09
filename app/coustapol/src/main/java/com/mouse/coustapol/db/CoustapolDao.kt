a6d6f5f0a1d6e9e58083950573d4256a97fba4ab
Oleh Shatov <lexusshatov2@gmail.com>
2022-11-09 11:43:30 +0200

    Ignore case when searching

--- app/coustapol/src/main/java/com/mouse/coustapol/db/CoustapolDao.kt
+++ app/coustapol/src/main/java/com/mouse/coustapol/db/CoustapolDao.kt
@@ -6,15 +6,19 @@ import androidx.room.Query
 @Dao
 interface CoustapolDao {
 
-    @Query("SELECT * FROM Country WHERE Name=:query OR Abbreviate=:query")
-    fun findCountry(query: String): Country?
-
-    @Query("SELECT * FROM State WHERE State.Country=:country")
-    fun findCountryStates(country: String): List<State>
-
-    @Query("SELECT * FROM State WHERE State.Country=(SELECT Abbreviate FROM Country WHERE Name=:country OR Abbreviate=:country) AND (Name=:query OR Abbreviate=:query)")
-    fun findState(country: String, query: String): State?
-
     @Query("SELECT * FROM Country")
     fun getCountries(): List<Country>
+
+    @Query("SELECT * FROM Country " +
+            "WHERE UPPER(Name)=UPPER(:query) " +
+            "OR UPPER(Abbreviate)=UPPER(:query)")
+    fun findCountry(query: String): Country?
+
+    @Query("SELECT * FROM State " +
+            "WHERE UPPER(State.Country)=(SELECT Abbreviate FROM Country WHERE UPPER(Name)=UPPER(:country) OR UPPER(Abbreviate)=UPPER(:country)) " +
+            "AND (UPPER(Name)=UPPER(:query) OR UPPER(Abbreviate)=UPPER(:query))")
+    fun findState(country: String, query: String): State?
+
+    @Query("SELECT * FROM State WHERE UPPER(State.Country)=UPPER(:country)")
+    fun findCountryStates(country: String): List<State>
 }
